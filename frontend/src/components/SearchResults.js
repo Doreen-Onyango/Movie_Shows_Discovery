a5763bf8350029f2e0c4ee16840852fa606b9005
doonyango <datieno001@gmail.com>
2025-06-24 22:07:21 +0300

    ref: remove duplicate span

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -149,7 +149,6 @@ const SearchResults = () => {
                   watchlist={watchlist}
                   onWatchlistChange={handleWatchlistChange}
                 />
-                <span className={`absolute top-2 right-2 px-2 py-1 text-xs rounded bg-primary-600 text-white font-semibold uppercase`}>{movie.media_type === 'tv' ? 'TV' : 'Movie'}</span>
               </div>
             ))}
           </div>
